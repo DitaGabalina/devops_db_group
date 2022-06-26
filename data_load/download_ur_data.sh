e4c171500d26b119353f22d6ffbca75140fca96c
Dita Gabalina <dita.gabalina2@sunberry.lv>
2022-06-26 17:51:24 +0300

    Datamodel, file download and first import scripts (CQL)

--- data_load/download_ur_data.sh
+++ data_load/download_ur_data.sh
@@ -1,4 +1,4 @@
-curl https://dati.ur.gov.lv/register/register.csv --output register.csv --silent --insecure
-curl https://dati.ur.gov.lv/register/ppi_public_persons_institutions.csv --output ppi_public_persons_institutions.csv --silent --insecure
-curl https://dati.ur.gov.lv/register/register_name_history.csv --output register_name_history.csv --silent --insecure
-curl https://dati.ur.gov.lv/register/ppi_delegated_entities.csv --output ppi_delegated_entities.csv --silent --insecure
+curl https://dati.ur.gov.lv/register/register.csv --output ~/external_data/register.csv --silent --insecure
+curl https://dati.ur.gov.lv/register/ppi_public_persons_institutions.csv --output ~/external_data/ppi_public_persons_institutions.csv --silent --insecure
+curl https://dati.ur.gov.lv/register/register_name_history.csv --output ~/external_data/register_name_history.csv --silent --insecure
+curl https://dati.ur.gov.lv/register/ppi_delegated_entities.csv --output ~/external_data/ppi_delegated_entities.csv --silent --insecure

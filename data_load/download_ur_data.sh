85948666e3525e99c81a605c2e8465b05582f30b
Dita Gabalina <dita.gabalina2@sunberry.lv>
2022-06-26 14:29:29 +0300

    Downloading UR data with curl

---
+++ data_load/download_ur_data.sh
@@ -0,0 +1,4 @@
+curl https://dati.ur.gov.lv/register/register.csv --output register.csv --silent --insecure
+curl https://dati.ur.gov.lv/register/ppi_public_persons_institutions.csv --output ppi_public_persons_institutions.csv --silent --insecure
+curl https://dati.ur.gov.lv/register/register_name_history.csv --output register_name_history.csv --silent --insecure
+curl https://dati.ur.gov.lv/register/ppi_delegated_entities.csv --output ppi_delegated_entities.csv --silent --insecure

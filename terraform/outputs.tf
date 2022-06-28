198039580714586cdaf16d3d078a091aadd93288
Rolands Jankovskis <Rolands.jankovskis@gmail.com>
2022-06-28 23:07:04 +0300

    Remove unnecesary lines / comments

--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-/* resource "local_file" "ansible_inventory" {
-  content = templatefile("./assets/templates/hosts.tpl", {
-    hosts =  module.apache-ec2.*.public_dns
-  })
-  filename = "./assets/inventory/hosts.cfg"
-} */

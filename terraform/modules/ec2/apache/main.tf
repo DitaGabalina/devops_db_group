d81d147fbe6444bc26b0bd72aa3a81e7c1df99f3
Rolands Jankovskis <72339907+whyme35@users.noreply.github.com>
2022-06-30 01:31:21 +0300

    Feature/10 casandra data load (#20)
    • Added a playbook for loading creating data model and loading data in main Cassandra node using shell and cql scripts provided by DitaGablina
    • Added Playbook execution in terraform for automated deployment
    • Fixed typos in hosts template

--- terraform/modules/ec2/apache/main.tf
+++ terraform/modules/ec2/apache/main.tf
@@ -63,7 +63,8 @@ provisioner "remote-exec" {
       "mkdir ~/ansible-codes",
       "git clone -b development https://github.com/DitaGabalina/devops_db_group.git ~/ansible-codes/devops_db_group",
       "ansible-playbook ~/ansible-codes/devops_db_group/ansible/apache_server-setup.yaml --ssh-common-args='-o StrictHostKeyChecking=accept-new'",
-      "ansible-playbook ~/ansible-codes/devops_db_group/ansible/cassandra_node-setup.yaml --ssh-common-args='-o StrictHostKeyChecking=accept-new'"
+      "ansible-playbook ~/ansible-codes/devops_db_group/ansible/cassandra_node-setup.yaml --ssh-common-args='-o StrictHostKeyChecking=accept-new'",
+      "ansible-playbook ~/ansible-codes/devops_db_group/ansible/cassandra_main_node-data_load.yaml"
     ]
 
   connection {

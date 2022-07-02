666c6c5fe046773f99940fa3260d09f44ee11373
Rolands Jankovskis <72339907+whyme35@users.noreply.github.com>
2022-07-03 02:03:13 +0300

    Feature/8 ansible for casandra configuration (#23)
    • Created a template with variables to replace cassandra.yaml
    • Created a playbook to get ip addreses from cassandra nodes and set them in template that overrides cassandra configuration files

--- terraform/modules/ec2/apache/main.tf
+++ terraform/modules/ec2/apache/main.tf
@@ -64,7 +64,8 @@ provisioner "remote-exec" {
       "git clone -b development https://github.com/DitaGabalina/devops_db_group.git ~/ansible-codes/devops_db_group",
       "ansible-playbook ~/ansible-codes/devops_db_group/ansible/apache_server-setup.yaml --ssh-common-args='-o StrictHostKeyChecking=accept-new'",
       "ansible-playbook ~/ansible-codes/devops_db_group/ansible/cassandra_node-setup.yaml --ssh-common-args='-o StrictHostKeyChecking=accept-new'",
-      "ansible-playbook ~/ansible-codes/devops_db_group/ansible/cassandra_main_node-data_load.yaml"
+      "ansible-playbook ~/ansible-codes/devops_db_group/ansible/cassandra_main_node-data_load.yaml",
+      "ansible-playbook ~/ansible-codes/devops_db_group/ansible/cassandra-cluster-creation.yaml"
     ]
 
   connection {
